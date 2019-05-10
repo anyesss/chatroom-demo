9a25253421415d84588f218ce68872c837968d62
xierongren <xierongren@foxmail.com>
2019-05-10 15:00:42 +0800

    修改ui+测试基本逻辑

--- chatRoomService.php
+++ chatRoomService.php
@@ -25,18 +25,41 @@ class chatRoom
 
     private function openHandle(swoole_websocket_server $server, $request)
     {
+        foreach ($this->server->connections as $fd) {
+            // 需要先判断是否是正确的websocket连接，否则有可能会push失败
+            if ($this->server->isEstablished($fd)) {
+                $this->server->push($fd, $this->sysPush("欢迎用户：{$request->fd}"));
+            }
+        }
+
         echo "server: handshake success with fd {$request->fd}\n";
     }
 
     private function msgHandle(Swoole\WebSocket\Server $server, $frame)
     {
+        foreach ($this->server->connections as $fd) {
+            // 需要先判断是否是正确的websocket连接，否则有可能会push失败
+            if ($this->server->isEstablished($fd)) {
+                $this->server->push($fd, $this->userPush($frame->data));
+            }
+        }
+
         echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-        $server->push($frame->fd, "this is server");
     }
 
     private function closeHandle($ser, $fd)
     {
         echo "client {$fd} closed\n";
+    }
+
+    private function sysPush($msg)
+    {
+        return json_encode(['type' => 0, 'msg' => '系统：' . $msg]);
+    }
+
+    private function userPush($msg)
+    {
+        return json_encode(['type' => 1, 'msg' => '用户：' .  htmlspecialchars($msg)]);
     }
 
 }
